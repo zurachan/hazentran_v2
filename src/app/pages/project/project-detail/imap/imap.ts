469d88fe136eb3be765229f661800a0e1b3cb4fc
duonght <manchestervn1996@gmail.com>
2025-08-07 01:11:18 +0700

    hoàn thành màn mplis, imap

--- src/app/pages/project/project-detail/imap/imap.ts
+++ src/app/pages/project/project-detail/imap/imap.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imap',
   templateUrl: './imap.html',
   styleUrl: './imap.css',
+  imports: [TranslateModule],
 })
 export class Imap implements OnInit {
   constructor() {}
--- src/app/pages/project/project-detail/mplis/mplis.ts
+++ src/app/pages/project/project-detail/mplis/mplis.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-mplis',
   templateUrl: './mplis.html',
-  styleUrl: './mplis.css'
+  styleUrl: './mplis.css',
+  imports: [TranslateModule],
 })
 export class Mplis implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
