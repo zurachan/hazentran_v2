2d5a8d46825da399798e47454a33d45ad45106e7
duonght <manchestervn1996@gmail.com>
2025-08-08 01:22:04 +0700

    hoàn thành màn hình cake

--- src/app/pages/project/project-detail/redeli/redeli.ts
+++ src/app/pages/project/project-detail/redeli/redeli.ts
@@ -9,4 +9,10 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class Redeli {
   constructor() {}
+  download() {
+    window.open(
+      'https://drive.google.com/file/d/1keSpdhT9R3nc8pelqAvM_5q2bo9miT2p/view',
+      '_blank'
+    );
+  }
 }
